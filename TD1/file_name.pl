b44a91371b1355d91566096d01eee4fe18daf75d
Alex Ballet <alexandre.ballet@keylightech.com>
2017-09-21 13:20:09 +0200

    seance 2 - indexation finie

--- TD1/file_name.pl
+++ TD1/file_name.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/perl -w
 use File::Basename;
+use utf8;
 
 my $output_file = "corpus.xml";
 
@@ -21,6 +22,14 @@ foreach $file (@docs) {
 
 	$nb_images = 0;
 
+	$is_reading_text_image_paragraph = 0;
+
+	$is_reading_legende_image = 0;
+
+	$text = "";
+
+	$legende = "";
+
 	print OUTPUT_FILE "<bulletin>\n";
 
 	# ------ Name of file ------
@@ -28,6 +37,8 @@ foreach $file (@docs) {
 	print OUTPUT_FILE "<fichier>";
 
 	$name = basename($file);
+
+	$name = replace_accents($name);
 
 	print OUTPUT_FILE "$name";
 
@@ -69,7 +80,9 @@ foreach $file (@docs) {
 
 				print OUTPUT_FILE "<titre>";
 
-				print OUTPUT_FILE "$+{title}";
+				$title = replace_accents("$+{title}");
+
+				print OUTPUT_FILE "$title";
 
 				print OUTPUT_FILE "</titre>\n";
 
@@ -83,7 +96,9 @@ foreach $file (@docs) {
 
 			print OUTPUT_FILE "<rubrique>";
 
-			print OUTPUT_FILE "$+{rubrique}";
+			$rubrique = replace_accents("$+{rubrique}");
+
+			print OUTPUT_FILE "$rubrique";
 
 			print OUTPUT_FILE "</rubrique>\n";
 
@@ -91,28 +106,31 @@ foreach $file (@docs) {
 
 		} elsif ($line =~ /<p class="style96"><span class="style95">/) {
 
+
 			$line =~ qr/<p class="style96"><span class="style95">(?'texte'.*)<\/span><\/p><\/td>/;
 
 			# ------ Text ------
 
-			print OUTPUT_FILE "<texte>";
-
 			if ($+{texte}) {
 
-				print OUTPUT_FILE "$+{texte}";
+				$text = $text . "$+{texte}";
 
 			} else {
 
 				$line =~ qr/<p class="style96"><span class="style95">(?'texte'.*)<br \/>/;
 
-				print OUTPUT_FILE "$+{texte}";
-			}
+				$text = $text . "$+{texte}";
 
-			print OUTPUT_FILE "</texte>\n";
+				$is_reading_text_image_paragraph = 1;
+			}
 
 			# ------ Texte ------
 
 		} elsif ($line =~ /<\/span><div style="text-align: center"><img src="/) {
+
+			if ($has_found_image) {
+				$nb_images = $nb_images + 1;
+			}
 
 			$line =~ qr/<\/span><div style="text-align: center"><img src="(?'url'.+)" border.*<br \/>/;
 
@@ -120,21 +138,97 @@ foreach $file (@docs) {
 
 			$has_found_image = 1;
 
-			next;
+		} elsif ($line =~ /<span class="style21"><strong>/) {
 
-		} elsif ($has_found_image && $line =~ /<span class="style21"><strong>/) {
+			if ($has_found_image) {
 
-			$line =~ qr/<span class="style21"><strong>(?'legende'.*)<\/strong>.*$/;
+				if ($line =~ qr/<span class="style21"><strong>(?'legende'.*)<\/strong>.*$/) {
 
-			$images_legende[$nb_images] = $+{legende};
-		}
+					$images_legende[$nb_images] = $+{legende};
 
-		if ($has_found_image) {
+					$has_found_image = 0;
+					$nb_images = $nb_images + 1;
 
-			$has_found_image = 0;
-			$nb_images = $nb_images + 1;
+				} elsif ($line =~ qr/<span class="style21"><strong>(?'legende'.*)<br \/>$/) {
+
+					$legende = "";
+
+					$legende = $legende . "$+{legende}";
+
+					$is_reading_legende_image = 1;
+				}
+
+			} else {
+
+				print("Has found legende but has not found image : $name");
+			}
+		} elsif ($line =~ qr/.*<\/p>.*$/ && $is_reading_text_image_paragraph) {
+
+				$is_reading_text_image_paragraph = 0;
+
+		} elsif ($line =~ qr/(?'legende'.*)<\/strong><br \/><\/span>$/ && $is_reading_legende_image) {
+
+				$legende = $legende . "$+{legende}";
+
+				$images_legende[$nb_images] = $legende;
+
+				$has_found_image = 0;
+				$nb_images = $nb_images + 1;
+
+				$is_reading_legende_image = 0;
+
+		} elsif ($line =~ qr/<span class="style88">(?'credits'.*)<\/span><\/div><span class="style95">.*$/) {
+
+
+		} elsif ($line =~ qr/<p class="style44"><span class="style85">(?'name'.*)<a href="mailto:.*">(?'mail'.*)<\/a><\/span>.*$/) {
+
+			# ------ Contact ------
+
+			print OUTPUT_FILE "<contact>";
+
+			$string = "$+{name}" . "$+{mail}";
+
+			$string = replace_accents("$string");
+
+			print OUTPUT_FILE "$string";
+
+			print OUTPUT_FILE "</contact>\n";
+
+			# ------ Contact ------
+
+		} elsif ($is_reading_text_image_paragraph) {
+
+			if ($is_reading_legende_image) {
+
+				$line =~ qr/(?'legende'.*)$/;
+
+				$legende = $legende . "$+{legende}";
+
+			} else {
+
+				$line =~ qr/(?'texte'.*)$/;
+
+				$text = $text . "$+{texte}";
+			}
 		}
 }
+
+# ------ Text ------
+
+print OUTPUT_FILE "<texte>";
+
+$text =~ s/<br \/>//g;
+$text =~ s/<\/strong>//g;
+$text =~ s/<strong \/>//g;
+$text =~ s/<strong>//g;
+
+$text = replace_accents("$text");
+
+print OUTPUT_FILE "$text";
+
+print OUTPUT_FILE "</texte>\n";
+
+# ------ Texte ------
 
 # ------ Images ------
 
@@ -142,7 +236,7 @@ print OUTPUT_FILE "<images>\n";
 
 if ($nb_images >= 1) {
 
-	for (my $i=0; $i <= $nb_images; $i++) {
+	for (my $i=0; $i < $nb_images; $i++) {
 
 	print OUTPUT_FILE "<image>\n";
 
@@ -154,7 +248,12 @@ if ($nb_images >= 1) {
 
 	print OUTPUT_FILE "<legendeImage>";
 
-	print OUTPUT_FILE "$images_legende[$i]";
+	if (scalar($images_legende[$i])) {
+
+		$legende = replace_accents("$images_legende[$i]");
+
+		print OUTPUT_FILE "$legende";
+	}
 
 	print OUTPUT_FILE "</legendeImage>\n";
    
@@ -168,8 +267,45 @@ print OUTPUT_FILE "</images>\n";
 # ------ Images ------
 
 print OUTPUT_FILE "</bulletin>\n";
+
 }
 
 print OUTPUT_FILE "</corpus>\n";
 
-close OUTPUT_FILE
+close OUTPUT_FILE;
+
+sub replace_accents {
+
+	$accentsTxt = $_[0];
+
+	$accentsTxt =~ s/&nbsp;/ /g;
+	$accentsTxt =~ s/&ecirc;/ê/g;
+	$accentsTxt =~ s/&ouml;/ö/g;
+	$accentsTxt =~ s/&ucirc;/û/g;
+	$accentsTxt =~ s/&egrave;/è/g;
+	$accentsTxt =~ s/&icirc;/î/g;
+	$accentsTxt =~ s/&eacute;/é/g;
+    $accentsTxt =~ s/&quot;/'/g;
+    $accentsTxt =~ s/&agrave;/à/g;
+    $accentsTxt =~ s/&acirc;/â/g;
+    $accentsTxt =~ s/&iuml;/ï/g;
+    $accentsTxt =~ s/&ocirc;/ô/g;
+    $accentsTxt =~ s/&ccedil;/ç/g;
+    $accentsTxt =~ s/&euml;/ë/g;
+    $accentsTxt =~ s/&ugrave;/ù/g;
+    $accentsTxt =~ s/&szlig;/ß/g;
+    $accentsTxt =~ s/&uuml;/ü/g;
+    $accentsTxt =~ s/&amp;/et/g;
+    $accentsTxt =~ s/&deg;/°/g;
+    $accentsTxt =~ s/&ograve;/ò/g;
+    $accentsTxt =~ s/&ecirc;/ê/g;
+    $accentsTxt =~ s/&#338;/Œ/g;
+    $accentsTxt =~ s/&#8211/—/g;
+    $accentsTxt =~ s/&#21;/--/g;
+	$accentsTxt =~ s/&nbsp;/ /g;
+	$accentsTxt =~ s/&nbsp;/ /g;
+	$accentsTxt =~ s/&nbsp;/ /g;
+	$accentsTxt =~ s/<.+?>/ /g;
+
+	return $accentsTxt;
+}
